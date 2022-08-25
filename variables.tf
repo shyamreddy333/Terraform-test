99fb772cb2d8d60461b9a15774559bf56ece301c
shyamreddy333 <112067859+shyamreddy333@users.noreply.github.com>
2022-08-25 21:47:09 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,10 +10,6 @@ variable "app_insights_name"{
     type = string
     description = "app_insights_name"
 }
-variable "law_id"{
-    type = bool
-    description = "log analytics ID"
-}
 variable "law_name"{
     type = string
     description = "log analytics name"
@@ -22,16 +18,5 @@ variable "sc_service_name"{
     type = string
     description = "app name"
 }
-variable "app_subnet_id"{
-    type = bool
-    description = "app_subnet_id"
-}
-variable "service_runtime_subnet_id"{
-    type = bool
-    description = "service_runtime_subnet_id"
-}
-variable "sc_cidr"{
-    type = list(string)
-    description = "sc_cidr"
-}
+
 
