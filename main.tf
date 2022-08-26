a43afe3bdf6c0e5af7ff2f551bf303a270500f96
shyamreddy333 <112067859+shyamreddy333@users.noreply.github.com>
2022-08-26 12:36:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,7 @@ resource "azurerm_monitor_diagnostic_setting" "diag" {
 }
 
 resource "azurerm_log_analytics_workspace" "sc_law" {
-  name                = "Dev_logAnalyticsWorkspace"
+  name                = "${var.law_name}"
   location            = "${var.rglocation}"
   resource_group_name = "${var.rgname}"
   sku                 = "PerGB2018"
@@ -65,7 +65,7 @@ resource "azurerm_log_analytics_workspace" "sc_law" {
 }
 
 resource "azurerm_spring_cloud_service" "sc" {
-  name                = "spring_app_name" 
+  name                = "${var.sc_service_name}" 
   resource_group_name = "${var.rgname}"
   location            = "${var.rglocation}"
   
