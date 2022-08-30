7b051148e9116c027e66d86bb2fac3b5efbe05e2
shyamreddy333 <112067859+shyamreddy333@users.noreply.github.com>
2022-08-30 13:07:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -30,5 +30,9 @@ variable "sc_bastion_host"{
     type = string
     description = "sc_bastion_host"
 }
+variable "azure_bastion_subnet"{
+    type = string
+    description = "azure_bastion_subnet name"
+}
 
 
